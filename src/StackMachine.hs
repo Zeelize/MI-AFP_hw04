f8f8edcaeac7ff29040d0e6a4ead8c6ea1028605
Vojtech Mraz <mraz.vojta@gmail.com>
2018-03-24 11:43:36 +0100

    Stack machine - logic done, still errors bad

--- src/StackMachine.hs
+++ src/StackMachine.hs
@@ -33,14 +33,27 @@ programRunner s m d (EOP) inp outp = outp
 programRunner s m d ((TA add) `Then` prg) inp outp = programRunner (Stack.push (Left add) s) m d prg inp outp
 programRunner s m d ((TV val) `Then` prg) inp outp = programRunner (Stack.push (Right val) s) m d prg inp outp
 
-programRunner s m d (DR `Then` prg) inp outp = undefined
-programRunner s m d (ST `Then` prg) inp outp = undefined
+programRunner s m d (DR `Then` prg) inp outp = programRunner (Stack.push (Right val) ns) m d prg inp outp
+    where 
+        add = topThatAddress s
+        ns = Stack.pop s
+        val = takeValueFromMemory add m
+            where
+                takeValueFromMemory :: Address -> Memory -> Value
+                takeValueFromMemory a mem = case mem Map.!? a of
+                    Nothing -> error "Uninitialized memory"
+                    Just v -> v 
+
+programRunner s m d (ST `Then` prg) inp outp = programRunner (Stack.pop ns) nm d prg inp outp
+    where
+        val = topThatValue s
+        ns = Stack.pop s
+        add = topThatAddress ns
+        nm = Map.insert add val m
 
 programRunner s m d (WR `Then` prg) inp outp = programRunner (Stack.pop s) m d prg inp noutp
     where 
-        noutp = case (Stack.top s) of
-            (Left _) -> error "Not value"
-            (Right val) -> outp Seq.|> val 
+        noutp = outp Seq.|> (topThatValue s) 
 
 programRunner s m d (RD `Then` prg) inp outp 
     | inp == Seq.empty = error "No input"
@@ -48,11 +61,76 @@ programRunner s m d (RD `Then` prg) inp outp
         where
             nv = Seq.index inp 0
 
-programRunner s m d (AD `Then` prg) inp outp = undefined
+programRunner s m d (AD `Then` prg) inp outp = programRunner nss m d prg inp outp
+    where
+        val1 = topThatValue s
+        ns = Stack.pop s
+        val2 = topThatValue ns
+        nss = Stack.push (Right (val1 + val2)) (Stack.pop ns)        
 
-programRunner s m d (SB `Then` prg) inp outp = undefined
-programRunner s m d (MT `Then` prg) inp outp = undefined
-programRunner s m d (DI `Then` prg) inp outp = undefined
-programRunner s m d ((JU lbl) `Then` prg) inp outp = undefined
-programRunner s m d ((JZ lbl) `Then` prg) inp outp = undefined
-programRunner s m d (lbl `Marks` prg) inp outp = undefined
+programRunner s m d (SB `Then` prg) inp outp = programRunner nss m d prg inp outp
+    where
+        val1 = topThatValue s
+        ns = Stack.pop s
+        val2 = topThatValue ns
+        nss = Stack.push (Right (val1 - val2)) (Stack.pop ns)        
+
+programRunner s m d (MT `Then` prg) inp outp = programRunner nss m d prg inp outp
+    where
+        val1 = topThatValue s
+        ns = Stack.pop s
+        val2 = topThatValue ns
+        nss = Stack.push (Right (val1 * val2)) (Stack.pop ns)        
+
+programRunner s m d (DI `Then` prg) inp outp = programRunner nss m d prg inp outp
+    where
+        val1 = topThatValue s
+        ns = Stack.pop s
+        val2 = topThatValue ns
+        nss = Stack.push (Right (mDivider val1 val2)) (Stack.pop ns)
+            where 
+                mDivider :: Value -> Value -> Value
+                mDivider _ 0 = error "Division by 0"
+                mDivider a b = div a b
+
+programRunner s m d ((JU lbl) `Then` prg) inp outp = programRunner s m nd nprg inp outp
+    where 
+        nd = findMarking d lbl prg
+        nprg = case nd Map.!? lbl of
+            Nothing -> error "Unknown label"
+            Just p -> p
+
+programRunner s m d ((JZ lbl) `Then` prg) inp outp 
+    | val == 0 = programRunner ns m d nprg inp outp
+    | otherwise = programRunner ns m d prg inp outp
+    where 
+        val = topThatValue s
+        ns = Stack.pop s
+        nprg =  (JU lbl) `Then` prg
+
+programRunner s m d (lbl `Marks` prg) inp outp = programRunner s m nd prg inp outp
+    where
+        nd = Map.insert lbl prg d
+
+-- Another helper functions
+topThatValue :: ComputerStack -> Value
+topThatValue s = case (Stack.topSafe s) of
+                    Nothing -> error "Empty stack"
+                    Just (Left _) -> error "Not value" 
+                    Just (Right val) -> val 
+
+topThatAddress :: ComputerStack -> Address
+topThatAddress s = case (Stack.topSafe s) of
+                    Nothing -> error "Empty stack"
+                    Just (Left add) -> add 
+                    Just (Right _) -> error "Not address"  
+                                     
+findMarking :: SubprogramDir -> Label -> Program -> SubprogramDir
+findMarking d l EOP = d
+findMarking d l (_ `Then` prg) = findMarking d l prg
+findMarking d l (lbl `Marks` prg) 
+    | (Map.member l d) = d
+    | l == lbl = Map.insert lbl prg d
+    | otherwise = findMarking nextD l prg
+        where 
+            nextD = Map.insert lbl prg d 
